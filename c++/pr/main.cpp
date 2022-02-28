ec0627d1b18a41bc145e42db68a491cee5a0fec8
tomtanner <trashicon3@gmail.com>
2022-02-28 17:11:33 +0530

    added notes

--- c++/pr/main.cpp
+++ c++/pr/main.cpp
@@ -1,91 +1,19 @@
 #include <iostream>
 using namespace std;
-class node
-{
+class node{
     public:
     int data;
-    node* next;
+    node *next;
     node(int d)
     {
-       data=d;
-       next=NULL;
+        data=d;
+        next=NULL;
     }
 };
-int len(node* head)
-{
-    if(head==NULL)
-        return 0;
-    node *temp=head;
-    int count=0;
-    while(temp!=NULL)
-    {
-        temp=temp->next;
-        count++;
-    }
-    return count;
-}
-void insertattail(node* &head,int item)
-{
-    node* n=new node(item);
-    if(head==NULL)
-        {
-            head=n;
-            return;
-        }
-    node* temp=head;
-    while(temp->next!=NULL)
-        temp=temp->next;
-    temp->next=n;
-}
-int intersection(node* &head1,node* head2)
-{
-    int l1=len(head1);
-    int l2=len(head2);
-    int d=0;
-    node *ptr1;
-    node *ptr2;
-    if(l1>l2)
-    {
-        d=l1-l2;
-        ptr1=head1;
-        ptr2=head2;
-    }
-    else//l2>l1
-    {
-        d=l2-l1;
-        ptr1=head2;
-        ptr2=head1;
-    }
-    while(d)
-    {
-        ptr1=ptr1->next;
-        d--;
-    }
-    while(ptr1!=NULL && ptr2!=NULL)
-    {
-        if(ptr1==ptr2)
-            return ptr1->data;
-      ptr1=ptr1->next;
-      ptr2=ptr2->next;
-    }
-    return -1;
-}
-void intersect(node* &head1,node* &head2,int pos)
-{
-    node* temp1=head1;
-    pos--;
-    while(pos--)
-        temp1=temp1->next;
-    node* temp2=head2;
-    while(temp2->next!=NULL)
-        temp2=temp2->next;
-    temp2->next=temp1;
-}
 void display(node* head)
 {
-    if(head==NULL)
-    {
-        cout<<"Linked List is empty"<<endl;
+    if(head==NULL){
+        cout<<"The list is empty"<<endl;
         return;
     }
     while(head!=NULL)
@@ -93,67 +21,83 @@ void display(node* head)
         cout<<head->data<<"->";
         head=head->next;
     }
-    cout<<endl;
+    cout<<"NULL"<<endl;
 }
-node* merge(node* head1,node* head2)
+void insertattail(node* &head,int item)
 {
-   node* p1=head1;
-   node* p2=head2;
-   node* dummy=new node(-1);
-   node* p3=dummy;
-   while(p1!=NULL && p2!=NULL)
+    node* n=new node(item);
+    if(head==NULL)
+    {
+        head=n;
+        return;
+    }
+    node* temp=head;
+    while(temp->next!=NULL)
+     temp=temp->next;
+     temp->next=n;
+}
+int length(node* head)
+{
+    int count=0;
+    while(head!=NULL)
+    {
+        head=head->next;
+        count++;
+    }
+   return count;
+}
+int intersection(node* head1,node* head2)
+{
+  int l1=length(head1);
+  int l2=length(head2);
+  if(l1>l2){
+    l1=l1-l2;
+    while(l1--)
+    {
+        head1=head1->next;
+    }
+  }
+   else{
+   l1=l2-l1;
+   while(l1--)
    {
-       if(p1->data<p2->data)
-       {
-        p3->next=p1;
-        p1=p1->next;
-       }
-       else
-       {
-           p3->next=p2;
-           p2=p2->next;
-       }
-       p3=p3->next;
+       head2=head2->next;
    }
-   while(p1!=NULL)
+   }
+   l2=0;
+   while(head1!=NULL )
    {
-       p3->next=p1;
-       p1=p1->next;
-       p3=p3->next;
+     if(head1==head2){
+         return head1->data;
+     }
+     head1=head1->next;
+     head2=head2->next;
+     l2++;
    }
-   while(p2!=NULL)
-   {
-       p3->next=p2;
-       p2=p2->next;
-       p3=p3->next;
-   }
-
-   return dummy->next;
+   return -1;
+}
+void intersector(node* head1,node* head2,int pos)
+{
+    pos--;
+while(pos--)
+    head1=head1->next;
+    while(head2->next!=NULL)
+    head2=head2->next;
+    head2->next=head1;
 }
 int main()
 {
- node* h1=NULL;
- node* h2=NULL;
- int l1,l2;
- cout<<"Size of the first linked list:"<<endl;
- cin>>l1;
- cout<<"Size of the second linked list:"<<endl;
- cin>>l2;
- int a1[l1],a2[l2];
- cout<<"Enter the elements of the first linked list"<<endl;
- for(int i=0;i<l1;i++)
- {
-     cin>>a1[i];
-     insertattail(h1,a1[i]);
- }
- cout<<"Enter the elements of the second linked list"<<endl;
- for(int i=0;i<l2;i++)
- {
-     cin>>a2[i];
-     insertattail(h2,a2[i]);
- }
- display(h1);
- display(h2);
- node* newhead=merge(h1,h2);
- display(newhead);
+    node *head1=NULL,*head2=NULL;
+    insertattail(head1,10);
+    insertattail(head1,20);
+    insertattail(head1,30);
+    insertattail(head1,40);
+    insertattail(head2,1);
+    insertattail(head2,2);
+    insertattail(head2,3);
+    intersector(head1,head2,2);
+    display(head1);
+    display(head2);
+    cout<<intersection(head1,head2);
+    return 0;
 }
