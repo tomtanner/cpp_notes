9b16fbbc63b0285495739ca1361131161a1b0c64
tomtanner <trashicon3@gmail.com>
2022-03-09 13:06:27 +0530

    auto

--- c++/pr/main.cpp
+++ c++/pr/main.cpp
@@ -1,32 +1,31 @@
+//using one conventional stack and one system stack
 #include <iostream>
+#include <stack>
 using namespace std;
-class node{
-    public:
-    int data;
-    node* next;
-    node(int val)
-    {
-        data=val;
-        next=NULL;
-    }
-};
 class queue{
-    node* front;
-    node* back;
-    public:
-    queue(){
-        front=NULL;
-        back=NULL;
-    }
-    void push(int x)
-    {
-        node* n=new node(x);
-        front
-        back->next=n;
-        back=n;
-    }
+	stack<int>s1;
+	public:
+	void push(int x)
+	{
+		s1.push(x);
+	}
+	int pop()
+	{
+		if(s1.empty())
+		{
+			cout<<"Queue is empty"<<endl;
+			return -1;
+		}
+		int res=s1.top();
+		s1.pop();
+        if(s1.empty())
+		return res;
+		int item=pop();
+		s.push(res);
+		return item;
+	}
 }
 int main()
 {
-    return 0;
+	return 0;
 }
